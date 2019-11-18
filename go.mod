ee9914496b690e9f8c3c5708f67e265df365c47e
Devin Carraway <git@devin.com>
2019-11-17 16:53:33 -0800

    initial import

---
+++ go.mod
@@ -0,0 +1,5 @@
+module main
+
+go 1.13
+
+require github.com/mxschmitt/golang-combinations v1.0.0 // indirect
---
+++ permute.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+	"sync"
+
+	combos "github.com/mxschmitt/golang-combinations"
+)
+
+var dictOnly = flag.Bool("dictonly", true, "Only return permutations from the dictionary")
+var dict = flag.String("dict", "/usr/share/dict/words", "Path to dictionary file")
+var anyLength = flag.Bool("any-length", false, "Generate combos of any nonzero subset length")
+
+var dictionary = map[string]struct{}{}
+var dictLoaded sync.Once
+
+func isDictWord(w string) bool {
+	dictLoaded.Do(func() {
+		f, err := os.Open(*dict)
+		if err != nil {
+			panic(fmt.Sprintf("%s: %v", *dict, err))
+		}
+		defer f.Close()
+		scanner := bufio.NewScanner(f)
+		for scanner.Scan() {
+			dictionary[scanner.Text()] = struct{}{}
+		}
+	})
+	_, exists := dictionary[w]
+	return exists
+}
+
+func permutations(arr []rune) [][]rune {
+	var helper func([]rune, int)
+	res := [][]rune{}
+
+	helper = func(arr []rune, n int) {
+		if n == 1 {
+			tmp := make([]rune, len(arr))
+			copy(tmp, arr)
+			res = append(res, tmp)
+		} else {
+			for i := 0; i < n; i++ {
+				helper(arr, n-1)
+				if n%2 == 1 {
+					tmp := arr[i]
+					arr[i] = arr[n-1]
+					arr[n-1] = tmp
+				} else {
+					tmp := arr[0]
+					arr[0] = arr[n-1]
+					arr[n-1] = tmp
+				}
+			}
+		}
+	}
+	helper(arr, len(arr))
+	return res
+}
+
+func main() {
+	flag.Parse()
+	if flag.NArg() < 1 || flag.Arg(0) == "" {
+		fmt.Println("usage: permute letters")
+		os.Exit(2)
+	}
+	input := flag.Arg(0)
+	bases := []string{input}
+	if *anyLength {
+		bases = []string{}
+		for _, c := range combos.All(strings.Split(input, "")) {
+			bases = append(bases, strings.Join(c, ""))
+		}
+	}
+	for _, w := range bases {
+		letters := make([]rune, len(w))
+		for i, c := range w {
+			letters[i] = c
+		}
+		for _, word := range permutations(letters) {
+			if *dictOnly && !isDictWord(string(word)) {
+				continue
+			}
+			fmt.Println(string(word))
+		}
+	}
+}
